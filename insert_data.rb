e512acd7b6e52c8fbe2b936314938502e58ac2e3
Thiago Rodrigues Santos <trsantos1@gmail.com>
2015-10-08 21:33:06 -0300

    Extrai dados do Stack Overflow Careers
    insert_data gera a lista de INSERTs nas tabelas do BD com os dados do Stack Overflow. Falta retirar os dados do Glassdoor agora, mas esta parte será mais fácil.
    
    so.rss é o arquivo de teste (estático). Apenas para todo mundo conseguir obter a mesma saída do script.
    
    Mais uma vez, preciso testar tudo isso melhor. Na verdade estou escrevendo tudo baseado no SQL que aprendi no livro... Mas vou rodar tudo isso no SQLite e/ou no PostgreSQL daqui a pouco.

---
+++ insert_data.rb
@@ -0,0 +1,70 @@
+require 'nokogiri'
+require 'json'
+require 'set'
+require 'open-uri'
+
+companies = Set.new
+
+companies_ids = {}
+offers_ids = {}
+skills_ids = {}
+
+doc = Nokogiri::XML(File.open('so.rss'))
+
+id = 1
+doc.css('item').each do |i|
+  offer_id = id
+  
+  url = i.css('link').children
+  company = i.css('a10|author a10|name').children.to_s
+  title = i.css('title').children
+  description = i.css('description').children
+  pub_date = i.css('pubDate').children
+  updated = i.css('a10|updated').children
+  location = i.css('location').children
+
+  unless companies_ids[company]
+    puts "INSERT INTO COMPANY(Id, CName) VALUES (#{id}, '#{company}');" 
+    companies_ids[company] = id
+    id += 1
+  end
+
+  puts "INSERT INTO OFFER(Id, Title, Description, Location, Url, PubDate, Updated, CompanyId) VALUES (#{offer_id}, '#{title}', '#{description}', '#{location}', '#{url}', '#{pub_date}', '#{updated}', #{companies_ids[company]});"
+
+  puts "INSERT INTO PROPOSES(CompanyId, OfferId) VALUES (#{companies_ids[company]}, #{offer_id});"
+
+  i.css('category').each do |c|
+    sname = c.children.to_s
+    unless skills_ids[sname]
+      puts "INSERT INTO SKILL(Id, SName) VALUES (#{id}, '#{sname}');"
+      skills_ids[sname] = id
+      id += 1
+    end
+    puts "INSERT INTO DEMANDS(OfferId, SkillId) VALUES (#{offer_id}, #{skills_ids[sname]});"
+  end
+
+  puts
+  id += 1
+end
+
+# companies.each do |c|
+#   j = JSON.parse open("http://api.glassdoor.com/api/api.htm?t.p=42773&t.k=jd0Cb9TfM3M&format=json&v=1&action=employers&q=#{c}").read
+#   e = j['response']['employers'].first
+#   if e
+#     puts e['name']
+#     puts e['website']
+#     puts e['industry']
+#     puts e['numberOfRatings']
+#     puts e['squareLogo']
+#     puts e['overallRating']
+#     puts e['cultureAndValuesRating']
+#     puts e['seniorLeadershipRating']
+#     puts e['compensationAndBenefitsRating']
+#     puts e['workLifeBalanceRating']
+#     puts e['recommendToFriendRating']
+#     puts e['ceo']['pctApprove'] if e['ceo']
+#   else
+#     puts "Company #{c} not found."
+#   end
+#   puts
+# end
