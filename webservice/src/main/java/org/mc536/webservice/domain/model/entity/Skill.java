7df512c057ba0b9525d91c7ee2f0208f5b46789e
Thiago Rodrigues Santos <trsantos1@gmail.com>
2015-11-21 20:53:43 -0200

    Estabelece relação entre as entidades User e Skill

--- webservice/src/main/java/org/mc536/webservice/domain/model/entity/Skill.java
+++ webservice/src/main/java/org/mc536/webservice/domain/model/entity/Skill.java
@@ -1,6 +1,17 @@
 package org.mc536.webservice.domain.model.entity;
 
-import javax.persistence.*;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import org.mc536.webservice.domain.model.entity.User;
 
 @Entity
 @Table(name = "Skill")
@@ -13,6 +24,14 @@ public class Skill {
 
     @Column(name = "SName", length = 30, unique = true, nullable = false)
     private String name;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+        name = "User_Skill",
+        joinColumns = { @JoinColumn(name = "SkillId", nullable = false) },
+        inverseJoinColumns = { @JoinColumn(name = "UserId", nullable = false)}
+    )
+    private List<User> users;
 
     public Integer getId() {
         return id;
@@ -28,5 +47,13 @@ public class Skill {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
     }
 }
